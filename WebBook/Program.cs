59c34a7891f72663b99d0d8ef09819dcd8dcb1a1
SAMA <SAMA@DESKTOP-C3RLPBD>
2023-09-06 03:42:17 +0300

    Add project files.

---
+++ WebBook/Program.cs
@@ -0,0 +1,51 @@
+using Infrastructure.Data;
+using Infrastructure.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<FreeBookDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("BookConnection")));
+            builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<FreeBookDbContext>();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+               endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Account}/{action=Login}/{id?}"
+                );
+              endpoints.MapControllerRoute(
+              name: "default",
+              pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+          
+
+            app.Run();
+        }
+    }
+}
